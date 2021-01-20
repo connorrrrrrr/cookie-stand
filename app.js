4bf02fe062d5dd41b43edb70cdb6fed1fbdc3fb9
Connor Curtis <concur1998@gmail.com>
2021-01-20 01:15:19 -0800

    made my objects display their data into a table instead of list items

--- app.js
+++ app.js
@@ -21,21 +21,22 @@ var seattle = {
   newArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   getRandomArbitrary: function () {
     //return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-    for (var i = 0; i < this.newArray.length; i++)  {
-      this.cookieArray.push (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+    for (var i = 0; i < this.newArray.length; i++) {
+      this.cookieArray.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
     }
   },
-  renderToDom: function ()  {
-    this.getRandomArbitrary ();
-    var seattleList = document.getElementById ("seattleList")
+  renderToDom: function () {
+    this.getRandomArbitrary();
+    //var seattleList = document.getElementById ("seattleList")
+    var seattleTable = document.getElementById("seattleTable")
     for (var i = 0; i < this.cookieArray.length; i++) {
-      var newLi = document.createElement ('li');
-      newLi.textContent = this.newArray[i] +  ': ' + this.cookieArray[i] + ' cookies'
-      seattleList.appendChild (newLi)
+      var newTd = document.createElement('td');
+      newTd.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
+      seattleTable.appendChild(newTd)
     }
   }
 }
-seattle.renderToDom ();
+seattle.renderToDom();
 
 var tokyo = {
   minCustomer: 3,
@@ -45,21 +46,22 @@ var tokyo = {
   newArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   getRandomArbitrary: function () {
     //return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-    for (var i = 0; i < this.newArray.length; i++)  {
-      this.cookieArray.push (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+    for (var i = 0; i < this.newArray.length; i++) {
+      this.cookieArray.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
     }
   },
-  renderToDom: function ()  {
-    this.getRandomArbitrary ();
-    var tokyoList = document.getElementById ("tokyoList")
+  renderToDom: function () {
+    this.getRandomArbitrary();
+    //var tokyoList = document.getElementById ("tokyoList")
+    var tokyoTable = document.getElementById("tokyoTable")
     for (var i = 0; i < this.cookieArray.length; i++) {
-      var newLi = document.createElement ('li');
-      newLi.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
-      tokyoList.appendChild (newLi)
+      var newTd = document.createElement('td');
+      newTd.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
+      tokyoTable.appendChild(newTd)
     }
   }
 }
-tokyo.renderToDom ();
+tokyo.renderToDom();
 
 var dubai = {
   minCustomer: 11,
@@ -69,21 +71,22 @@ var dubai = {
   newArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   getRandomArbitrary: function () {
     //return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-    for (var i = 0; i < this.newArray.length; i++)  {
-      this.cookieArray.push (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+    for (var i = 0; i < this.newArray.length; i++) {
+      this.cookieArray.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
     }
   },
-  renderToDom: function ()  {
-    this.getRandomArbitrary ();
-    var dubaiList = document.getElementById ("dubaiList")
+  renderToDom: function () {
+    this.getRandomArbitrary();
+    // var dubaiList = document.getElementById ("dubaiList")
+    var dubaiTable = document.getElementById("dubaiTable")
     for (var i = 0; i < this.cookieArray.length; i++) {
-      var newLi = document.createElement ('li');
-      newLi.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
-      dubaiList.appendChild (newLi)
+      var newTd = document.createElement('td');
+      newTd.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
+      dubaiTable.appendChild(newTd)
     }
   }
 }
-dubai.renderToDom ();
+dubai.renderToDom();
 
 var paris = {
   minCustomer: 20,
@@ -93,21 +96,22 @@ var paris = {
   newArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   getRandomArbitrary: function () {
     //return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-    for (var i = 0; i < this.newArray.length; i++)  {
-      this.cookieArray.push (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+    for (var i = 0; i < this.newArray.length; i++) {
+      this.cookieArray.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
     }
   },
-  renderToDom: function ()  {
-    this.getRandomArbitrary ();
-    var parisList = document.getElementById ("parisList")
+  renderToDom: function () {
+    this.getRandomArbitrary();
+    // var parisList = document.getElementById ("parisList")
+    var parisTable = document.getElementById("parisTable")
     for (var i = 0; i < this.cookieArray.length; i++) {
-      var newLi = document.createElement ('li');
-      newLi.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
-      parisList.appendChild (newLi)
+      var newTd = document.createElement('td');
+      newTd.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
+      parisTable.appendChild(newTd)
     }
   }
 }
-paris.renderToDom ();
+paris.renderToDom();
 
 var lima = {
   minCustomer: 2,
@@ -117,20 +121,21 @@ var lima = {
   newArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   getRandomArbitrary: function () {
     //return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-    for (var i = 0; i < this.newArray.length; i++)  {
-      this.cookieArray.push (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+    for (var i = 0; i < this.newArray.length; i++) {
+      this.cookieArray.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
     }
   },
-  renderToDom: function ()  {
-    this.getRandomArbitrary ();
-    var limaList = document.getElementById ("limaList")
+  renderToDom: function () {
+    this.getRandomArbitrary();
+    // var limaList = document.getElementById ("limaList")
+    var limaTable = document.getElementById("limaTable")
     for (var i = 0; i < this.cookieArray.length; i++) {
-      var newLi = document.createElement ('li');
-      newLi.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
-      limaList.appendChild (newLi)
+      var newTd = document.createElement('td');
+      newTd.textContent = this.newArray[i] + ': ' + this.cookieArray[i] + ' cookies'
+      limaTable.appendChild(newTd)
     }
   }
 }
-lima.renderToDom ();
+lima.renderToDom();
 
 
